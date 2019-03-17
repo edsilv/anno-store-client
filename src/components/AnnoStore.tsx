70bb30f4f1b56aaf9c4afe0543b0b3172e5d9389
edsilv <e.silverton@gmail.com>
2019-03-17 18:45:41 +0000

    added ionic/react

--- src/components/AnnoStore.tsx
+++ src/components/AnnoStore.tsx
@@ -1,6 +1,10 @@
 import React, { Component, ChangeEvent } from "react";
 import AnnoStoreQuery from "./AnnoStoreQuery";
 import { QueryType } from "../Enums";
+import { IonApp, IonItem, IonInput } from '@ionic/react';
+import '@ionic/core/css/core.css';
+import '@ionic/core/css/ionic.bundle.css';
+import { InputChangeEventDetail } from "@ionic/core";
 
 interface Props {
   endpoint: string;
@@ -53,8 +57,8 @@ export default class AnnoStore extends Component<Props, State> {
     };
   }
 
-  handleSecretChange(ev: ChangeEvent<HTMLInputElement>) {
-    this.setState({ secret: ev!.target!.value });
+  handleSecretChange(ev: CustomEvent<InputChangeEventDetail>) {
+    this.setState({ secret: ev!.detail!.value as string });
   }
 
   handleQueryTypeChange(ev: ChangeEvent<HTMLSelectElement>) {
@@ -87,63 +91,65 @@ export default class AnnoStore extends Component<Props, State> {
     if (endpoint) {
       return (
         <div>
-          <div>
-            <input
-              type="text"
-              placeholder="secret"
-              value={secret ? secret : ""}
-              onChange={this.handleSecretChange}
-              style={{ width: "440px" }}
+          <IonApp>
+            <IonItem>
+              <IonInput
+                type="text"
+                placeholder="secret"
+                value={secret ? secret : ""}
+                onIonChange={this.handleSecretChange}
+                style={{ width: "440px" }}
+              />
+            </IonItem>
+            <IonItem>
+              <input
+                type="text"
+                placeholder="id"
+                value={id ? id : ""}
+                onChange={this.handleIdChange}
+                style={{ width: "440px" }}
+              />
+            </IonItem>
+            <IonItem>
+              <select value={queryType} onChange={this.handleQueryTypeChange}>
+                <option value={QueryType.SAVE}>save</option>
+                <option value={QueryType.EDIT}>edit</option>
+                <option value={QueryType.DELETE}>delete</option>
+              </select>
+            </IonItem>
+            <IonItem>
+              <textarea
+                placeholder="anno json"
+                rows={10}
+                cols={60}
+                onChange={this.handleAnnotationChange}
+                value={annotation ? annotation : ""}
+              />
+            </IonItem>
+            <button
+              onClick={e =>
+                this.setState({
+                  queryTimestamp: String(new Date().getTime())
+                })
+              }
+            >
+              submit
+            </button>
+            {this.renderDebug()}
+            <AnnoStoreQuery
+              annotation={annotation}
+              endpoint={endpoint}
+              id={id}
+              queryTimestamp={queryTimestamp}
+              queryType={queryType}
+              secret={secret}
+              onQueryResult={(queryResult: string) => {
+                this.setState({
+                  queryResult: queryResult
+                });
+              }}
             />
-          </div>
-          <div>
-            <input
-              type="text"
-              placeholder="id"
-              value={id ? id : ""}
-              onChange={this.handleIdChange}
-              style={{ width: "440px" }}
-            />
-          </div>
-          <div>
-            <select value={queryType} onChange={this.handleQueryTypeChange}>
-              <option value={QueryType.SAVE}>save</option>
-              <option value={QueryType.EDIT}>edit</option>
-              <option value={QueryType.DELETE}>delete</option>
-            </select>
-          </div>
-          <div>
-            <textarea
-              placeholder="anno json"
-              rows={10}
-              cols={60}
-              onChange={this.handleAnnotationChange}
-              value={annotation ? annotation : ""}
-            />
-          </div>
-          <button
-            onClick={e =>
-              this.setState({
-                queryTimestamp: String(new Date().getTime())
-              })
-            }
-          >
-            submit
-          </button>
-          {this.renderDebug()}
-          <AnnoStoreQuery
-            annotation={annotation}
-            endpoint={endpoint}
-            id={id}
-            queryTimestamp={queryTimestamp}
-            queryType={queryType}
-            secret={secret}
-            onQueryResult={(queryResult: string) => {
-              this.setState({
-                queryResult: queryResult
-              });
-            }}
-          />
+          </IonApp>
         </div>
       );
     } else {
